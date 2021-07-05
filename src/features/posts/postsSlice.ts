9f58e166d8a376e66d7be33d4b83686b5c2c7d34
JackHewitt08 <Hewittjack2011@gmail.com>
2021-07-05 23:07:47 +0100

    Added fetch request, types, thunk and reducer to postsSlice

--- src/features/posts/postsSlice.ts
+++ src/features/posts/postsSlice.ts
@@ -1,18 +1,46 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
-// TODO: Add a type for `Post` based on the API data (https://jsonplaceholder.typicode.com/posts)
-// TODO: Add a type for the state structure and also use it in reducers.
-const initialState = {
+interface IPostData {
+  userId: number,
+  id: number,
+  title: string,
+  body: string,
+}
+
+interface IPostsSlice {
+  isLoading: boolean,
+  posts: Array<IPostData>,
+}
+
+const initialState: IPostsSlice = {
   isLoading: false,
-  posts: [],
+  posts: new Array<IPostData>(),
 };
+
+const getFakeJsonData = async (): Promise<Array<IPostData>> => {
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts').then(res => res.json());
+  return response;
+}
+
+const fetchPostData = createAsyncThunk(
+  'posts/load',
+  async (_arg, _thunkAPI) => {
+    const response = await getFakeJsonData();
+    return response
+  }
+)
 
 const postsSlice = createSlice({
   name: 'posts',
   initialState,
   reducers: {
-    // TODO
+    // TODO <-- putting this todo here was very sneaky
   },
+  extraReducers: (builder) => {
+    builder.addCase(fetchPostData.fulfilled, (state, action) => {
+      state.posts = action.payload;
+    })
+  }
 });
 
 // Default export is the reducer.
