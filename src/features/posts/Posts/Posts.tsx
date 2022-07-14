e4b9c5112d31756f1cc5c989826d82b613b420a8
Ben Hide <ben.m.hide@googlemail.com>
2022-07-14 11:30:22 +0100

    Fixed TODOs Dispatch action on mount to load post data, get post data loading state, updated Posts component

--- src/features/posts/Posts/Posts.tsx
+++ src/features/posts/Posts/Posts.tsx
@@ -1,44 +1,28 @@
-import React, { useMemo } from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { useAppDispatch, useAppSelector } from 'app/store';
 import { PostBody } from 'features/posts/PostBody/PostBody';
+import { getPosts, selectAllPosts } from 'features/posts/postsSlice';
 
 export interface PostsProps {}
 
 export const Posts: React.FC<PostsProps> = (): JSX.Element => {
-  // TODO: Dispatch an action when this component is mounted that loads the post data.
-  // TODO: Get the post data and loading state from the store instead of this test data.
-  const posts = useMemo(
-    () => [
-      {
-        userId: 1,
-        id: 1,
-        title:
-          'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
-        body:
-          'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto',
-      },
-      {
-        userId: 1,
-        id: 2,
-        title: 'qui est esse',
-        body:
-          'est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla',
-      },
-      {
-        userId: 1,
-        id: 3,
-        title: 'ea molestias quasi exercitationem repellat qui ipsa sit aut',
-        body:
-          'et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut',
-      },
-    ],
-    []
-  );
-  const isLoading = false;
+  const { posts, isLoading, error } = useAppSelector(selectAllPosts);
+  const dispatch = useAppDispatch();
 
-  return isLoading ? (
-    <p>'Loading…'</p>
-  ) : (
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [dispatch]);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Failed to fetch posts</p>;
+  }
+
+  return (
     <ul>
       {posts.map((post) => (
         <li key={post.id}>
